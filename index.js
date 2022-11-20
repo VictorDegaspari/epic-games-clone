140981529a9131f583ab05bb33e3251c9e2a1292
Victor Degaspari <vdegaspari.vm@gmail.com>
2022-11-20 20:03:27 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import usersRoutes from './src/database/Controller/usersController.js';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/users', usersRoutes);
 app.use(bodyParser.urlencoded({ extended: false }));
