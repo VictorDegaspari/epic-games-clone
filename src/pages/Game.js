0c2edbfba41b19a95f34852c9a875b2449f78213
Victor Degaspari <victor.degaspari@clinicarx.com.br>
2023-04-26 20:51:59 -0300

    Adicionando novas telas

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,37 +1,74 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { post } from "../js/index";
+
 export default function Game() {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({});
+    const baseUrl = "https://epic-games-clone-wheat.vercel.app";
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const jsonData = JSON.stringify(Object.fromEntries(data.entries()));
+    postGame(JSON.parse(jsonData));
+    console.log(jsonData);
+};
+
+const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+    ...prevData,
+    [name]: value,
+    }));
+};
+
+async function postGame(data){   
+        try {
+            setLoading(true);
+            const response = await post(baseUrl + '/games/post', data, false);
+            setLoading(false);
+            if (response.info?.type == 'Error') throw new Error();
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
+}
+    
     return (
         <>
-            <div id="show-game" className="flex flex-column w-100">
+            <div id="show-game" className="flex flex-column w-100 show">
                 <div className="game-edit flex-column"></div>
-                <div id="loader-game">
+                {loading && <div id="loader-game">
                     <div className="loader"></div>
-                </div>
+                </div>}
                 <form className="form-edit-game w-100 flex-column" encType="multipart/form-data"></form>
-                <form className="form-save-game w-100 flex-column" encType="multipart/form-data">
+                <form className="form-save-game w-100 flex-column show" onSubmit={ (event) => handleSubmit(event)} encType="multipart/form-data">
                         <h3>Cadastrar jogo</h3>
                         <label htmlFor="title" className="flex flex-column w-100">
                             Título
-                            <input name="title" type="text" placeholder="Título" />
+                            <input name="title" type="text" placeholder="Título" onChange={handleInputChange} />
                         </label>
                         <label htmlFor="current_price" className="flex flex-column w-100">
                             Preço
-                            <input name="current_price" type="number" placeholder="Preço" />
+                            <input name="current_price" type="number" placeholder="Preço" onChange={handleInputChange}/>
                         </label>
                         <label htmlFor="url" className="flex flex-column w-100">
                             Imagem
-                            <input name="url" type="text" placeholder="Imagem do jogo" />
+                            <input name="url" type="text" placeholder="Imagem do jogo" onChange={handleInputChange}/>
                         </label>
                         <label htmlFor="discount" className="flex flex-column w-100">
                             Desconto
-                            <input name="discount" type="number" placeholder="% do desconto" />
+                            <input name="discount" type="number" placeholder="% do desconto" onChange={handleInputChange}/>
                         </label>
                         <label htmlFor="old_price" className="flex flex-column w-100">
                             Preço antigo
-                            <input name="old_price" type="number" placeholder="Valor antigo do jogo" />
+                            <input name="old_price" type="number" placeholder="Valor antigo do jogo" onChange={handleInputChange}/>
                         </label>
                     <button type="submit">SALVAR</button>
                 </form>
-                <button className="w-100" id="go-back"><span>VOLTAR</span></button>
+                <button className="w-100" id="go-back" onClick={ () => navigate('/home')}><span>VOLTAR</span></button>
                 <small id="form-game-error" className="error"></small>
           </div>
         </>
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,31 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { post } from "../js/index";
+
 export default function User() {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({});
+    const baseUrl = "https://epic-games-clone-wheat.vercel.app";
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const jsonData = JSON.stringify(Object.fromEntries(data.entries()));
+        postUser(JSON.parse(jsonData));
+        console.log(jsonData);
+    };
+    
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+        }));
+    };
+
+    async function postUser(data){   
+        try {
+            setLoading(true);
+            const response = await post(baseUrl + '/users/post', data, false);
+            setLoading(false);
+            if (response.info?.type == 'Error') throw new Error();
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
+}
     return (
         <>
-            <div id="show-user" className="flex flex-column w-100">
-                <div id="loader-user">
-                    <div className="loader"></div>
-                </div>
-                <form className="form-save-user w-100 flex-column">
+            <div id="show-user" className="flex flex-column w-100 show">
+                { loading && <div id="loader-user">
+                    <div className="loader" ></div>
+                </div>}
+                <form className="form-save-user w-100 flex-column show" onSubmit={ (event) => handleSubmit(event)}>
                     <h3>Cadastrar usuário</h3>
                     <label htmlFor="name" className="flex flex-column w-100">
                         Nome
-                        <input name="name" type="text" placeholder="Nome" required/>
+                        <input name="name" type="text" placeholder="Nome" required onChange={handleInputChange}/>
                     </label>
                     <label htmlFor="email" className="flex flex-column w-100">
                         E-mail
-                        <input name="email" type="email" placeholder="E-mail" required/>
+                        <input name="email" type="email" placeholder="E-mail" required onChange={handleInputChange}/>
                     </label>
                     <label htmlFor="password" className="flex flex-column w-100">
                         Senha
-                        <input name="password" type="password" placeholder="Senha" required/>
+                        <input name="password" type="password" placeholder="Senha" required onChange={handleInputChange}/>
                     </label>
                     <label htmlFor="password_c" className="flex flex-column w-100">
                         Confirmar senha
-                        <input name="password_c" type="password" placeholder="Senha" required/>
+                        <input name="password_c" type="password" placeholder="Senha" required onChange={handleInputChange}/>
                     </label>
                     <button type="submit" className="w-100">SALVAR</button>
                 </form>
-                <button className="w-100" id="go-back-user"><span>VOLTAR</span></button>
+                <button className="w-100" id="go-back-user" onClick={ () => navigate('/home')}><span>VOLTAR</span></button>
                 <small id="form-user-error" className="error"></small>
           </div>
         </>
